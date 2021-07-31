a9bdeec86d96b94f6c0c18011e3c2335819201eb
Fernando_Ajset <fernando_ajset@hotmail.com>
2021-07-31 10:43:24 -0600

    "Agregando ejercicio de laboratorio1"

---
+++ 1.UMG/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+/**
+ *
+ * @author ferna
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Edgar Fernando");
+    }
+    
+}

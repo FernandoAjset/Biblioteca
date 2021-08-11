aa20c69def3b7bc9058e0cddf39c953feb5a478f
Fernando Ajset <fernando_ajset@hotmail.com>
2021-08-11 17:14:48 -0600

    Agregado script de clase31-07

---
+++ SQL/Clase31-07.sql
@@ -0,0 +1,9 @@
+show tables;
+#en oracle se usa el comando: select * from tabs#
+select * from actor;
+select actor_id,first_name, last_name from actor;
+
+#ejemplo consulta con where
+# select * from jobs where (min_salary + mas_salary)/2 >= 5000;
+
+ 

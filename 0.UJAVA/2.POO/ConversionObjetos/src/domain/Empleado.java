c55e7cc0ed56bc72c5adcfc5025419327440ef39
FerAjset <fernando_ajset@hotmail.com>
2021-08-26 20:09:29 -0600

    Agregados proyectos de UJAVA, Clases: InstanceOf, ConversionObjetos

--- 0.UJAVA/2.POO/ConversionObjetos/src/domain/Empleado.java
+++ 0.UJAVA/2.POO/ConversionObjetos/src/domain/Empleado.java
@@ -27,4 +27,11 @@ public class Empleado {
     public void setSueldo(double sueldo) {
         this.sueldo = sueldo;
     }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "nombre=" + nombre + ", sueldo=" + sueldo + '}';
+    }
+    
+    
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ﻿Repositorio principal de todo mi aprendizaje.
 Configurado y corriendo en Windows y Ubuntu
+
+Donaciónes: 
+    https://paypal.me/EAjset?locale.x=es_XC

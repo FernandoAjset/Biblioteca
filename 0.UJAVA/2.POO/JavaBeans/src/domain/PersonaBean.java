bf8074c7f7495df75d10d101f3e775b0031aaf35
FerAjset-Portatil <fernando_ajset@hotmail.com>
2021-09-07 02:25:58 -0600

    Ejercicio JavaBeans

---
+++ 0.UJAVA/2.POO/JavaBeans/src/domain/PersonaBean.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ferna
+ */
+public class PersonaBean implements Serializable{
+    private String nombre;
+    private String apellido;
+    
+    public PersonaBean (){
+        
+    }
+}

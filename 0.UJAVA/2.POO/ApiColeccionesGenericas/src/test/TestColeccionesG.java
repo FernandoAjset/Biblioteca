0c1b062a20bdee178dc08fad7cc97134bc3eb50f
FerAjset-Ubuntu <fernando_ajset@hotmail.com>
2021-09-13 13:18:08 -0600

    Agregado Proyecto de curso UJAVA: LaboratorioFinal

--- 0.UJAVA/2.POO/ApiColeccionesGenericas/src/test/TestColeccionesG.java
+++ 0.UJAVA/2.POO/ApiColeccionesGenericas/src/test/TestColeccionesG.java
@@ -2,7 +2,7 @@ package test;
 
 import java.util.*;
 
-public class TestColeccionesGenericas {
+public class TestColeccionesG {
 
     public static void main(String[] args) {
 
---
+++ 0.UJAVA/2.POO/LaboratorioFinal/src/gt/com/itxpert/peliculas/excepciones/LecturaDatosEx.java
@@ -0,0 +1,12 @@
+package gt.com.itxpert.peliculas.excepciones;
+
+/**
+ *
+ * @author ferajset
+ */
+public class LecturaDatosEx extends AccesoDatosEx{
+    
+    public LecturaDatosEx(String mensaje){
+        super(mensaje);
+    }
+}

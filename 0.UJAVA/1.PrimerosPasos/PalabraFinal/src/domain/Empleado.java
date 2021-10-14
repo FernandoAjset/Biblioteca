25215de55bb4a7518102e69ed95c41f855622c93
FerAjset-G3 <fernando_ajset@hotmail.com>
2021-10-14 14:00:43 -0600

    Reorganización de carpetas

--- 0.UJAVA/1.PrimerosPasos/PalabraFinal/src/domain/Empleado.java
+++
@@ -1,9 +0,0 @@
-
-package domain;
-
-public class Empleado{// extends Persona //NO SE PUEDE EXTENDER POR FINAL EN LA CLASE PADRE
-  
-//    public void imprimir(){ //SI SE INTENTA SOBREESCRIBIR MARCA ERROR POR FINAL
-//        System.out.println("Saludos");
-////    } 
-}
--- 0.UJAVA/1.PrimerosPasos/Paquetes/src/gt/com/itxpert/Utileria.java
+++
@@ -1,8 +0,0 @@
-
-package gt.com.itxpert;
-
-public class Utileria {
-    public static void imprimir(String s){
-        System.out.println("s = " + s);
-    }
-}
